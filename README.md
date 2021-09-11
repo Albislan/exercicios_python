4b78018b480e53b861fed2811c3554855d04edc6
Albislan <albislan@hotmail.com>
2021-09-11 18:42:38 -0300

    iniciando exercicios com strings

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # exercicios_python
 Exercicios com Gustavo Guanabara do curso em video python youtube
+utilizando o python 3 e VSCode
---
+++ mundo1/testes.py
@@ -0,0 +1,3 @@
+n = str(input('Digite uma frase \n'))
+print(f' Usando qual é o indice da letra i na frase {n}, o indice é {n.index("i")}\n')
+print(f' Removendo prefixo que no caso é a letra O da palavra {n}, {n.removeprefix("O")}\n')
