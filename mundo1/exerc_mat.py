90b0f6dc611185200b856ee75e0aa25bb65ceaba
Albislan <albislan@hotmail.com>
2021-09-12 17:28:59 -0300

    exercicios e testes aritimeticos

---
+++ mundo1/exerc_mat.py
@@ -0,0 +1,29 @@
+# from typing import Collection
+
+
+# a = 'Início'
+# b = 'Fim'
+# print(f'{a:*^50}')
+# n = int(input('Digite um número inteiro para visualizar o antecessor e o sucessor \n'))
+# print(f'O antecessor do número "{n}" é {n-1} e o sucessor é {n+1}')
+# print(f'{b:*^50}')
+
+
+# print(f'{a:*^50}')
+# n1 = float(input('Digite a primeira nota para saber a média\n'))
+# n2 = float(input('Digite a segunda nota para saber a média\n'))
+# print(f'Sua média nas notas é {(n1 + n2) / 2}')
+# print(f'{b:*^50}')
+
+
+# print(f'{a:*^50}')
+# n_tab = int(input('Digite um número inteiro para saber o resultado da tabuada desse número \n'))
+# print(f'A tabuada de "{n_tab}" é \n{n_tab*1} \n{n_tab*2} \n{n_tab*3} \n{n_tab*4} \n{n_tab*5} \n{n_tab*6} \n{n_tab*7} \n{n_tab*8} \n{n_tab*9} \n{n_tab*10}')
+# print(f'{b:*^50}')
+
+
+# print(f'{a:*^50}')
+# salario = float(input('Digite o seu salario atual para saber qual será o seu salário amanhã com um aumento de 15% \n'))
+# novo_salario = salario + salario * 0.15
+# print(f'Seu novo salário será de R$ "{novo_salario:.2f}" uhhuuuu')
+# print(f'{b:*^50}')
--- mundo1/testes.py
+++ mundo1/testes.py
@@ -1,3 +1,33 @@
-n = str(input('Digite uma frase \n'))
-print(f' Usando qual é o indice da letra i na frase {n}, o indice é {n.index("i")}\n')
-print(f' Removendo prefixo que no caso é a letra O da palavra {n}, {n.removeprefix("O")}\n')
+# n = str(input('Digite uma palavra de preferecia "Oi" para testes \n'))
+# print(f' Usando qual é o indice da letra i na frase {n}, o indice é {n.index("i")}\n')
+# print(f' Removendo prefixo que no caso é a letra O da palavra {n}, {n.removeprefix("O")}\n')
+
+#Strings end
+# ######################
+#Aritmético begin
+from typing import Collection
+
+
+a = 5
+b = 2
+
+print(f' o valor de {a} ao quadrado é {a**b}\nque é o mesmo que pow(5,2) = {pow(5,2)}') # equivale a 5 ao quadrado
+#ordem Precedencia
+#1 - ()
+#2 - **
+#3 - * / // %
+#4 - + -
+# a = 'Início'
+# b = 'Fim'
+# print(f'{a:*^50}') #centraliza
+# print(f' o valor da conta 3*5+4**2 é {3*5+4**2}')
+# print()
+# print(f' o valor do calculo 3*(5+4)**2 é {3*(5+4)**2}')
+# print()
+# print(f'raiz quadrada de 81 é int(81**(1/2)) {int(81**(1/2))}')
+# print()
+# print(f'raiz cúbica de 127 é int(127**(1/3)) {int(127**(1/3))}')
+# print()
+# print(f'{a:>20}') # sinal de > alinha a direita, sinal de < alinha a esquerda e o sinal ^ centraliza
+# print(f'{(4/3):.2f} é o resultado da divisão de 4/3 com formatação de 2 casas decimais') # formatando 2 casas decimais
+# print(f'{b:*^50}')
